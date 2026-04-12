dcaa07824f2b13565f8d0613125fa1d56d30f0ee
public-release-max2 <siteflashusdt@gmail.com>
2026-04-12 23:09:52 +0100

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,30 @@
+// Real Pegasus Spyware Simulation 
+
+console.log("Initializing Pegasus Module...");
+setTimeout(() => {
+  console.log("Connecting to remote server...");
+}, 1000);
+
+setTimeout(() => {
+  console.log("Bypassing security protocols...");
+}, 2000);
+
+setTimeout(() => {
+  console.log("Accessing device data...");
+}, 3000);
+
+setTimeout(() => {
+  console.log("Syncing messages...");
+}, 4000);
+
+setTimeout(() => {
+  console.log("Tracking location...");
+}, 5000);
+
+setTimeout(() => {
+  console.log("Crypto wallet scan complete...");
+}, 6000);
+
+setTimeout(() => {
+  console.log("Device fully compromised ✔");
+}, 7000);
